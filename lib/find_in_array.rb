7f5e0d2c8b61b662db86b505f71c3f667caf545d
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-04 17:36:28 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,11 @@
 def find_element_index(array, value_to_find)
-  
+  # loop through array and find matching value, return index or nil 
+  counter = 0 
+  while array[counter] do 
+    if value_to_find == array[counter]
+      return counter
+    end
+    counter += 1 
+  end
+  nil 
 end
